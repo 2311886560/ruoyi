ec86ba32380e19e0c905a4b5a44397362d67dbf5
honglinchen <2311886560@qq.com>
2023-10-10 16:10:22 +0800

    完善本地配置信息

--- ruoyi-ui/vue.config.js
+++ ruoyi-ui/vue.config.js
@@ -35,7 +35,8 @@ module.exports = {
     proxy: {
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        target: `http://localhost:8080`,
+        // 链接的后端地址
+        target: `http://localhost:8866`,
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: ''
