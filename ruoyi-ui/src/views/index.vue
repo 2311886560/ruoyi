1b3f2098451abdb2dc0ca46228abc49b74795901
honglinchen <2311886560@qq.com>
2023-12-25 11:03:54 +0800

    去除模版

--- ruoyi-ui/src/views/index.vue
+++ ruoyi-ui/src/views/index.vue
@@ -1,86 +1,14 @@
 <template>
   <div class="app-container home">
     <el-row :gutter="20">
-<!--      <el-col :sm="24" :lg="12" style="padding-left: 20px">-->
-<!--        <h2>工厂后台管理框架</h2>-->
-<!--        <p>-->
-<!--          一直想做一款后台管理系统，看了很多优秀的开源项目但是发现没有合适自己的。于是利用空闲休息时间开始自己写一套后台系统。如此有了xx管理系统，她可以用于所有的Web应用程序，如网站管理后台，网站会员中心，CMS，CRM，OA等等，当然，您也可以对她进行深度定制，以做出更强系统。所有前端后台代码封装过后十分精简易上手，出错概率低。同时支持移动客户端访问。系统会陆续更新一些实用功能。-->
-<!--        </p>-->
-<!--        <p>-->
-<!--          <b>当前版本:</b> <span>v{{ version }}</span>-->
-<!--        </p>-->
-<!--      </el-col>-->
-
+      <el-col :sm="24" :lg="12" style="padding-left: 20px">
+        <h2>南宁订单工厂管理系统</h2>
+        <p>
+          <b>当前版本:</b> <span>v{{ version }}</span>
+        </p>
+      </el-col>
       <el-col :sm="24" :lg="12" style="padding-left: 50px">
-        <el-row>
-          <el-col :span="12">
-            <h2>技术选型</h2>
-          </el-col>
-        </el-row>
-        <el-row>
-          <el-col :span="6">
-            <h4>后端技术</h4>
-            <ul>
-              <li>SpringBoot</li>
-              <li>Spring Security</li>
-              <li>JWT</li>
-              <li>MyBatis</li>
-              <li>Druid</li>
-              <li>Fastjson</li>
-              <li>...</li>
-            </ul>
-          </el-col>
-          <el-col :span="6">
-            <h4>前端技术</h4>
-            <ul>
-              <li>Vue</li>
-              <li>Vuex</li>
-              <li>Element-ui</li>
-              <li>Axios</li>
-              <li>Sass</li>
-              <li>Quill</li>
-              <li>...</li>
-            </ul>
-          </el-col>
-        </el-row>
-      </el-col>
-    </el-row>
-    <el-divider />
-    <el-row :gutter="20">
-      <el-col :xs="24" :sm="24" :md="12" :lg="8">
-        <el-card class="update-log">
-          <div slot="header" class="clearfix">
-            <span>联系信息</span>
-          </div>
-          <div class="body">
-            <p>
-              <i class="el-icon-s-promotion"></i> 百度一下：<el-link
-                href="https://www.baidu.com/"
-                target="_blank"
-                >https://www.baidu.com/</el-link
-              >
-            </p>
-          </div>
-        </el-card>
-      </el-col>
-      <el-col :xs="24" :sm="24" :md="12" :lg="8">
-        <el-card class="update-log">
-          <div slot="header" class="clearfix">
-            <span>更新日志</span>
-          </div>
-          <el-collapse accordion>
-            <el-collapse-item title="v1.1.0 - 2023-11-28">
-              <ol>
-                <li>工厂订单管理员管理订单、库存</li>
-              </ol>
-            </el-collapse-item>
-            <el-collapse-item title="v1.0.0 - 2023-11-22">
-              <ol>
-                <li>工厂前后端分离系统正式发布</li>
-              </ol>
-            </el-collapse-item>
-          </el-collapse>
-        </el-card>
+
       </el-col>
     </el-row>
   </div>
